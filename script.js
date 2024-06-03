807fe38e5de1361c5e298a807e7b5435622ec93d
Shamshad Begum <133691291+SyedaSB@users.noreply.github.com>
2024-06-04 05:10:59 +0530

    Add files via upload
    Developed a basic Rock-paper-scissors game using Html, CSS, and JavaScript.

---
+++ script.js
@@ -0,0 +1,122 @@
+var compscore = 0
+var userscore = 0
+
+var noofmatch = document.querySelector("pmatch")
+function play(clicked_id) {
+    noofmatch++
+    document.getElementById("pmatch").innerHTML = noofmatch
+
+    var compscore_span = document.querySelector("#comp-score")
+    var userscore_span = document.querySelector("#user-score")
+
+    var img = document.getElementById(clicked_id)
+    var clone = img.cloneNode(true)
+    document.getElementById("player").innerHTML = ""
+    document.getElementById("computer").innerHTML = ""
+    document.getElementById("player").appendChild(clone)
+
+    var reflect
+    if (img.id == "paper") {
+        reflect = 0
+    }
+    else if (img.id == "stone") {
+        reflect = 1
+    }
+    else if (img.id == "scissor") {
+        reflect = 2
+    }
+    console.log("You=" + reflect)
+
+    var num = Math.floor((Math.random() * 10) % 3)
+    switch (num) {
+        case 0:
+            var img = document.getElementById("paper")
+            var clone = img.cloneNode(true)
+            document.getElementById("computer").appendChild(clone)
+            break
+        case 1:
+            var img = document.getElementById("stone")
+            var clone = img.cloneNode(true)
+            document.getElementById("computer").appendChild(clone)
+            break
+        case 2:
+            var img = document.getElementById("scissor")
+            var clone = img.cloneNode(true)
+            document.getElementById("computer").appendChild(clone)
+            break
+    }
+    console.log("Computer=" + num)
+
+    if (num == reflect) {
+        document.getElementById("Result").innerHTML = "It's a Draw"
+    }
+    else if (num == 0) {
+        if (reflect == 1) {
+            compscore++
+            compscore_span.innerHTML = compscore
+            document.getElementById("Result").innerHTML = "ohh noo! You Lost"
+        }
+        else {
+            userscore++
+            userscore_span.innerHTML = userscore
+            document.getElementById("Result").innerHTML = "Congratulations! You Wonn.."
+        }
+    }
+    else if (num == 1) {
+        if (reflect == 0) {
+            userscore++
+            userscore_span.innerHTML = userscore
+            document.getElementById("Result").innerHTML = "Congratulations! U Wonn.."
+        }
+        else {
+            compscore++
+            compscore_span.innerHTML = compscore
+            document.getElementById("Result").innerHTML = "ohh noo! You Lost"
+        }
+    }
+    else if (num == 2) {
+        if (reflect == 0) {
+            compscore++
+            compscore_span.innerHTML = compscore
+            document.getElementById("Result").innerHTML = "ohh noo! You Lost"
+        }
+        else {
+            userscore++
+            userscore_span.innerHTML = userscore
+            document.getElementById("Result").innerHTML = "Congratulations! You Wonn.."
+        }
+    }
+    if (noofmatch == 5) {
+        noofmatch = 0
+        
+        var modal = document.getElementById("myModal");
+        var span = document.getElementsByClassName("close")[0];
+        modal.style.display = "block";
+        if(compscore==userscore){
+            document.getElementById("text").innerHTML="It's a Draw🤝 but what an Awesome match!!"
+        }
+        else if(compscore>userscore){
+            document.getElementById("text").innerHTML="Computer won this match 🤖. Call for a next match.. "
+        }
+        else if(compscore<userscore){
+            document.getElementById("text").innerHTML="Wohooo!! U Won the Match.. Belived in you "
+        }
+        
+
+
+        span.onclick = function() {
+            modal.style.display = "none";
+            setTimeout(reload,0)
+          }
+          window.onclick = function(event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+              setTimeout(reload,0)
+            }
+          }
+        
+    }
+}
+function reload(){
+    location.reload()
+}
